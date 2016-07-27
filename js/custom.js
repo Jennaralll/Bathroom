4f6de4b5b59216362b6027ee5e774354b961d23b
cindyhuynh <cindymei.huynh@gmail.com>
2016-07-27 09:26:21 -0700

    finished added all the location markers

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 function initMap() {
 	var mapDiv = document.getElementById('map');
 	var map = new google.maps.Map(mapDiv, {
-	    center: {lat: 37.783, lng: -122.446},
-	    zoom: 12
+	    center: {lat: 37.789, lng: -122.399},
+	    zoom: 15
 	});
 
-	map.setMapTypeId(google.maps.MapTypeId.HYBRID);
+	// map.setMapTypeId(google.maps.MapTypeId.);s
 
     // Function for adding a marker to the page.
     function addMarker(location) {
@@ -30,34 +30,55 @@ function initMap() {
 	}
 
     // Testing the addMarker function
-    var japanTown = new google.maps.LatLng(37.785, -122.429);
-    var japanTownMarker = addMarker(japanTown);
+    var noodz = new google.maps.LatLng(37.789671, -122.400564);
+    var noodzMarker = addMarker(noodz);
 
-	var goldenGate = new google.maps.LatLng(37.819, -122.478);
-	var goldengateMarker = addMarker(goldenGate);
+	var peetz = new google.maps.LatLng(37.784618, -122.406799);
+	var peetzMarker = addMarker(peetz);
 
-	var exploratorium = new google.maps.LatLng(37.800, -122.398);
-	var exploreMarker = addMarker(exploratorium);
+	var ghir = new google.maps.LatLng(37.788515, -122.402037);
+	var ghirMarker = addMarker(ghir);
 
-	var aTT = new google.maps.LatLng(37.778, -122.389);
-	var attPark = addMarker(aTT);
+	var bread = new google.maps.LatLng(37.790398, -122.399244);
+	var breadMarker = addMarker(bread);
 
-	var cT = new google.maps.LatLng(37.794, -122.407);
-	var chinaMarker = addMarker(cT);
+	var booty = new google.maps.LatLng(37.788803, -122.401672);
+	var bootyMarker = addMarker(booty);
 
-	var ggPark = new google.maps.LatLng(37.769, -122.486);
-	var gg = addMarker(ggPark);
+	var supburg = new google.maps.LatLng(37.786942, -122.404037);
+	var supburgMarker = addMarker(supburg);
 
-	var union = new google.maps.LatLng(37.787, -122.407);
-	var unionn = addMarker(union);
+	var oasis = new google.maps.LatLng(37.786861, -122.403697);
+	var oasisMarker = addMarker(oasis);
 
-	addInfoWindow(japanTownMarker, "Japantown");
-	addInfoWindow(goldengateMarker, "Golden Gate Bridge");
-	addInfoWindow(exploreMarker, "Exploratorium");
-	addInfoWindow(attPark, "AT&T Park");
-	addInfoWindow(chinaMarker, "Chinatown");
-	addInfoWindow(gg, "Golden Gate Park");
-	addInfoWindow(unionn, "Union Square");
+	var macys = new google.maps.LatLng(37.786959, -122.405973);
+	var macysMarker = addMarker(macys);
+
+	var port = new google.maps.LatLng(37.789791, -122.401145);
+	var portMarker = addMarker(port);
+
+	var oldnavy = new google.maps.LatLng(37.785320, -122.405972);
+	var oldnavyMarker = addMarker(oldnavy);
+
+	var ginto = new google.maps.LatLng(37.788335, -122.402948);
+	var gintoMarker = addMarker(ginto);
+
+	var maz = new google.maps.LatLng(37.787601, -122.404064);
+	var mazMarker = addMarker(maz);
+
+
+	addInfoWindow(noodzMarker, "Noodles & Company");
+	addInfoWindow(peetzMarker, "Peets Coffee and Company");
+	addInfoWindow(ghirMarker, "Ghirardelli");
+	addInfoWindow(breadMarker, "Andersen Bread");
+	addInfoWindow(bootyMarker, "Boudin Bakery");
+	addInfoWindow(supburgMarker, "Super Duper Burger");
+	addInfoWindow(macysMarker, "Macy's Men");
+	addInfoWindow(portMarker, "Portico Restaurant");
+	addInfoWindow(oldnavyMarker, "Old Navy");
+	addInfoWindow(gintoMarker, "Ginto Izakaya Japanaise");
+	addInfoWindow(mazMarker, "Mazarine Coffee");
+	addInfoWindow(oasisMarker, "Oasis Grill");
 	
 
 }
